4808f725e3b96dd6f51b2bcf5d475e9d993a918b
Matthew Harper <60448620+DaintyJet@users.noreply.github.com>
2023-11-28 20:45:46 -0500

    Init

---
+++ Install-Updates/DiscordUpdate.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "============================================="
+echo "               Unpacking Discord             "
+echo "============================================="
+
+if (( $(ls ~/Downloads | grep -i "discord.*.tar.gz" | wc -l) == 0)); then
+    echo "Please Put the tar.gz Discord file into the Downloads Folder!"
+    exit 1
+fi
+
+tar -xvzf "~Downloads/$(ls /Downloads | grep -i "discord*.tar.gz")" -C /opt
+
+echo "============================================="
+echo "               Updating Sym Link            "
+echo "============================================="
+
+if(( $(ls /opt | grep -i "Discord" | wc -l) == 0 )); then
+    echo "Failed to extract Discord!"
+    exit 1
+fi
+
+sudo ln -sf /opt/Discord/Discord /usr/bin/Discord
+
+
+echo "============================================="
+echo "           Updating Icon and Menu            "
+echo "============================================="
+
+sed -i "s#Exec=*#Exec=/usr/bin/Discord#g" /opt/Discord/discord.desktop
+sed -i "s#Icon=*#Icon=/opt/Discord/discord.png#g" /opt/Discord/discord.desktop
+cp -r /opt/Discord/discord.desktop /usr/share/applications
